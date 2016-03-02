a5a8b4c81c07f11b1e36687bc3eeb8318cb6ba81
Nicholas March <n.march213@gmail.com>
2016-03-01 20:09:41 -0600

    Login works with information taken from the database.
    Registration also functional.
    
    Added documentation.

--- AndroidClient/app/src/main/java/com/example/capstoneimsports/capstoneimsports/LoginActivity.java
+++ AndroidClient/app/src/main/java/com/example/capstoneimsports/capstoneimsports/LoginActivity.java
@@ -1,47 +1,29 @@
 package com.example.capstoneimsports.capstoneimsports;
 
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Button;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.EditText;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
+import com.example.capstoneimsports.capstoneimsports.server.ServerHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button bLogin,bRegister,bFacebookLogin;
-    EditText etUsername, etPassword;
+    EditText etEmail, etPassword;
+    JSONObject obj = new JSONObject();
+    ServerHandler server = new ServerHandler();
+    String url = "http://104.197.91.105:8080/api/user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -49,10 +31,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etUsername = (EditText)findViewById(R.id.etUsername);
+        etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText)findViewById(R.id.etPassword);
         bLogin = (Button)findViewById(R.id.login_button);
         bRegister = (Button)findViewById(R.id.register_button);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         bLogin.setOnClickListener(this);
         bRegister.setOnClickListener(this);
@@ -60,36 +45,70 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+    /**
+     * @param v This handles whenever someone clicked on the screen, depeding on whats clicked
+     */
     @Override
     public void onClick(View v)
     {
         switch(v.getId())
         {
+            //When the login is pressed
             case R.id.login_button:
-                onLogin();
+                try {
+                    onLogin();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
+            //When the register is clicked
             case R.id.register_button:
                 startActivity(new Intent(this,NewRegistration.class));
                 break;
+            //When facebook button is clicked
             case R.id.facebook_login_fragment:
                 startActivity(new Intent(this,Facebook_Login_Fragment.class));
                 break;
         }
     }
 
-    protected void onLogin()
-    {
-        if(etUsername.toString().equals("user") && etPassword.toString().equals("pass"))
-        {
-            Intent intent = new Intent(this, HomePage.class);
-            startActivity(intent);
-            Toast.makeText(LoginActivity.this, "Thanks for logging in!", Toast.LENGTH_SHORT).show();
+    /**
+     * @throws IOException
+     *
+     * When the login button is clicked, a user will begin the login process
+     */
+    protected void onLogin() throws IOException {
+
+        //Get email and pass from etFields
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        //Makes sure the fields are not empty
+        if (email.equals(null) || password.equals(null)) {
+            Toast.makeText(LoginActivity.this, "You must enter Email AND Password", Toast.LENGTH_SHORT).show();
+
         }
-        else
-        {
-            Toast.makeText(LoginActivity.this, "Wrong Information", Toast.LENGTH_SHORT).show();
+        //Put email and pass into JSON
+        try {
+            obj.put("email", email);
+            obj.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
+        //Begins login process through ServerHandler
+        String response = server.doLoginAuth(url, email, password);
+
+        //If logged in
+        if (response.equals("Authenticated")) {
+            Intent intent = new Intent(this, HomePage.class);
+            startActivity(intent);
+            Toast.makeText(LoginActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+        }
+        //Will show unauthorized in failed login
+        else {
+            Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
