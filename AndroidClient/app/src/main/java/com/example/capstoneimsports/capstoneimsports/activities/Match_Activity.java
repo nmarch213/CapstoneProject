c94a3f66b44df87ad08557be9c25fd383482f6d1
Nicholas March <n.march213@gmail.com>
2016-04-16 18:30:12 -0500

    BACKJKKKKKKKK THAT ASS UP

--- AndroidClient/app/src/main/java/com/example/capstoneimsports/capstoneimsports/activities/Match_Activity.java
+++ AndroidClient/app/src/main/java/com/example/capstoneimsports/capstoneimsports/activities/Match_Activity.java
@@ -2,19 +2,12 @@ package com.example.capstoneimsports.capstoneimsports.activities;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -22,22 +15,17 @@ import android.widget.TextView;
 import com.example.capstoneimsports.capstoneimsports.R;
 import com.example.capstoneimsports.capstoneimsports.fragments.Time_Fragment;
 import com.example.capstoneimsports.capstoneimsports.models.Match_model;
-import com.example.capstoneimsports.capstoneimsports.models.User_model;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class Match_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Match_Activity extends AppCompatActivity {
 
     public static Match_model match;
     TextView team_one_name, team_one_score, team_two_name, team_two_score, league, gameDate;
 
-    @Bind(R.id.toolbar)
+    @Bind(R.id.app_bar)
     Toolbar toolbar;
-    @Bind(R.id.drawer_layout)
-    DrawerLayout drawerLayout;
-    @Bind(R.id.nav_view)
-    NavigationView navigationView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -48,16 +36,9 @@ public class Match_Activity extends AppCompatActivity implements NavigationView.
 
 
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        );
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
         setDetails();
 
         final FrameLayout team1 = (FrameLayout) findViewById(R.id.match_fragment);
@@ -110,15 +91,6 @@ public class Match_Activity extends AppCompatActivity implements NavigationView.
                        // location[0], location[1] + team1.getHeight());
             }
         });
-
-        //How to change elements in the header programatically
-        View headerView = navigationView.getHeaderView(0);
-        TextView emailText = (TextView) headerView.findViewById(R.id.textView);
-        TextView headerText = (TextView) headerView.findViewById(R.id.headerName);
-        headerText.setText(User_model.getFirstName() + " " + User_model.getLastName());
-        emailText.setText(User_model.getEmail());
-
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public void setDetails() {
@@ -139,16 +111,6 @@ public class Match_Activity extends AppCompatActivity implements NavigationView.
         team_two_name.setText(match.getTeam_two_name());
         team_two_score = (TextView) findViewById(R.id.team_two_score);
         team_two_score.setText(String.valueOf(match.getTeam_two_score()));
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     public Dialog onCreateDialog() {
@@ -182,43 +144,6 @@ public class Match_Activity extends AppCompatActivity implements NavigationView.
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if (id == android.R.id.home) {
-            NavUtils.navigateUpFromSameTask(this);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_profile) {
-            Intent intent = new Intent(this, Profile_Activity.class);
-            startActivity(intent);
-            finish();
-        }
-        else if (id == R.id.nav_home) {
-            Home_Activity.getInstance().finish();
-            Intent intent = new Intent(this, Home_Activity.class);
-            startActivity(intent);
-            finish();
-        }
-        else if (id == R.id.nav_myTeams) {
-
-        }
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
